5d541fa30f3070d03f8c5fd03c15fc447df4bd24
Mireyamc <95554473+Mireyamc@users.noreply.github.com>
2024-05-20 01:34:57 -0400

    Update pregunta10.py

--- pregunta10.py
+++ pregunta10.py
@@ -1,41 +1,37 @@
 from multiprocessing import *
 
-def generate_series(start, end, queue):
-    result = []
-    for i in range(start, end):
-        result.append((i + 1) * 2)
-    queue.put(result)
+def generar_s(ini, fin_r, cola_s):
+    resultado = []
+    for i in range(ini, fin_r):
+        resultado.append((i + 1) * 2)
+    queue.put(resultado)
 
 if __name__ == '__main__':
-    N = 24  # Número de términos en la serie
-    num_processes = 6  # Número de procesos
+    N = 24  
+    num_processes = 6  
 
-    terminos_por_proceso = N // num_processes
-    queue = Queue()
-    processes = []
+    terminos_por_proceso = N 
+    queue = queue()
+    procesos = []
 
-    # Crear y arrancar los procesos
-    for rank in range(num_processes):
-        inicio = rank * terminos_por_proceso
+    for rango in range(num_processes):
+        inicio = rango * terminos_por_proceso
         fin = inicio + terminos_por_proceso
-        if rank == num_processes - 1:
+        if rango == num_processes - 1:
             fin = N
-        p = Process(target=generate_series, args=(inicio, fin, queue))
-        processes.append(p)
+        p = Process(target=generar_s, args=(inicio, fin, queue))
+        procesos.append(p)
         p.start()
 
-    # Recopilar los resultados
-    results = []
+    resultados = []
     for _ in range(num_processes):
-        results.append(queue.get())
+        resultados.append(queue.get())
 
-    # Esperar a que todos los procesos terminen
-    for p in processes:
+    for p in procesos:
         p.join()
 
-    # Imprimir los resultados en el formato deseado
     print("Salida:")
     vector_num = 1
-    for result in results:
-        print(f"Vector {vector_num}: {' '.join(map(str, result))}")
+    for resultado in resultados:
+        print(f"Vector {vector_num}: {' '.join(map(str, resultado))}")
         vector_num += 1
