5d05f8e1948fe53cb81e95ab08c6523c06b14ffd
Mireyamc <95554473+Mireyamc@users.noreply.github.com>
2024-05-20 01:36:11 -0400

    Update pregunta13.py

--- pregunta13.py
+++ pregunta13.py
@@ -1,6 +1,5 @@
 import multiprocessing
 
-# Función de Fibonacci con memoización
 def fibonacci(n, memo={}):
     if n in memo:
         return memo[n]
@@ -11,7 +10,6 @@ def fibonacci(n, memo={}):
         memo[n] = resultado
         return resultado
 
-# Función para calcular la secuencia de Fibonacci en un rango dado
 def calcular_fibonacci_rango(inicio, fin):
     return [fibonacci(i) for i in range(inicio, fin)]
 
@@ -20,11 +18,9 @@ def main():
     num_procesadores = multiprocessing.cpu_count()
     terminos_por_procesador = num_terminos // num_procesadores
 
-    # Usar Pool de procesos para ejecutar en paralelo
     with multiprocessing.Pool(processes=num_procesadores) as pool:
         resultados = pool.starmap(calcular_fibonacci_rango, [(i*terminos_por_procesador, (i+1)*terminos_por_procesador) for i in range(num_procesadores)])
 
-    # Unir resultados de todos los procesos
     secuencia_fibonacci = []
     for resultado in resultados:
         secuencia_fibonacci.extend(resultado)
